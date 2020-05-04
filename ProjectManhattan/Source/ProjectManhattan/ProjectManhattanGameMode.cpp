39d9584d661b71c3f92c5f436dff8a0493091a66
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2020-05-04 07:48:32 -0700

    01 - Voxel Runner Setup

---
+++ ProjectManhattan/Source/ProjectManhattan/ProjectManhattanGameMode.cpp
@@ -0,0 +1,22 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "Engine/World.h"
+#include "ProjectManhattanGameMode.h"
+#include "ProjectManhattanCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+
+AProjectManhattanGameMode::AProjectManhattanGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
+
+void AProjectManhattanGameMode::SpawnTile()
+{
+	//GetWorld()->SpawnActor()
+}
